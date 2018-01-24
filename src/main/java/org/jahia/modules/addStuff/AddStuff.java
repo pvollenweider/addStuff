14d751aa9bf4534764636f339fdf1f9abaa407ee
Philippe Vollenweider <pvollenweider@jahia.com>
2018-01-24 11:24:49 +0100

    Add new properties to be able to add stuff on the begining / end of the head / body tag Change the filter priority so we ensure that the end of the head tag is at the end of the head (and not before the static assets)

--- src/main/java/org/jahia/modules/addStuff/AddStuff.java
+++ src/main/java/org/jahia/modules/addStuff/AddStuff.java
@@ -1,6 +1,7 @@
 package org.jahia.modules.addStuff;
 
 import java.util.List;
+
 import net.htmlparser.jericho.*;
 import org.apache.commons.lang.StringUtils;
 import org.jahia.services.render.RenderContext;
@@ -21,11 +22,22 @@ public class AddStuff extends AbstractFilter implements ApplicationListener<Appl
     @Override
     public String execute(String previousOut, RenderContext renderContext, Resource resource, RenderChain chain) throws Exception {
         String out = previousOut;
+        String addStuffHeadTop = renderContext.getSite().hasProperty("addStuffHeadTop") ? renderContext.getSite().getProperty("addStuffHeadTop").getString() : null;
         String addStuffHead = renderContext.getSite().hasProperty("addStuffHead") ? renderContext.getSite().getProperty("addStuffHead").getString() : null;
+        String addStuffBodyTop = renderContext.getSite().hasProperty("addStuffBodyTop") ? renderContext.getSite().getProperty("addStuffBodyTop").getString() : null;
         String addStuffBody = renderContext.getSite().hasProperty("addStuffBody") ? renderContext.getSite().getProperty("addStuffBody").getString() : null;
-        if (StringUtils.isNotEmpty(addStuffHead) || StringUtils.isNotEmpty(addStuffBody)) {
+        if (StringUtils.isNotEmpty(addStuffHead) || StringUtils.isNotEmpty(addStuffBodyTop) || StringUtils.isNotEmpty(addStuffBody)) {
             Source source = new Source(previousOut);
             OutputDocument outputDocument = new OutputDocument(source);
+            if (StringUtils.isNotBlank(addStuffHeadTop)) {
+                List<Element> headElementList = source.getAllElements(HTMLElementName.HEAD);
+                for (Element element : headElementList) {
+                    final StartTag headStartTag = element.getStartTag();
+                    outputDocument.replace(headStartTag.getBegin()+headStartTag.toString().indexOf(">"), headStartTag.getBegin()+headStartTag.toString().indexOf(">") + 1,
+                            ">\n" + AggregateCacheFilter.removeEsiTags(addStuffHeadTop) + "\n");
+                    break; // avoid to loop if for any reasons multiple body in the page
+                }
+            }
             if (StringUtils.isNotBlank(addStuffHead)) {
                 List<Element> headElementList = source.getAllElements(HTMLElementName.HEAD);
                 for (Element element : headElementList) {
@@ -36,13 +48,20 @@ public class AddStuff extends AbstractFilter implements ApplicationListener<Appl
                     break; // avoid to loop if for any reasons multiple body in the page
                 }
             }
+            if (StringUtils.isNotBlank(addStuffBodyTop)) {
+                List<Element> bodyElementList = source.getAllElements(HTMLElementName.BODY);
+                for (Element element : bodyElementList) {
+                    final StartTag bodyStartTag = element.getStartTag();
+                    outputDocument.replace(bodyStartTag.getBegin()+bodyStartTag.toString().indexOf(">"), bodyStartTag.getBegin()+bodyStartTag.toString().indexOf(">") + 1,
+                            ">\n" + AggregateCacheFilter.removeEsiTags(addStuffBodyTop) + "\n");
+                    break; // avoid to loop if for any reasons multiple body in the page
+                }
+            }
             if (StringUtils.isNotBlank(addStuffBody)) {
                 List<Element> bodyElementList = source.getAllElements(HTMLElementName.BODY);
-                for (Element element : bodyElementList ) {
+                for (Element element : bodyElementList) {
                     final EndTag bodyEndTag = element.getEndTag();
-
-                    outputDocument.replace(bodyEndTag.getBegin(), bodyEndTag.getBegin() + 1,
-                            "\n" + AggregateCacheFilter.removeEsiTags(addStuffBody) + "\n<");
+                    outputDocument.replace(bodyEndTag.getBegin(), bodyEndTag.getBegin() + 1, "\n" + AggregateCacheFilter.removeEsiTags(addStuffBody) + "\n<");
                     break; // avoid to loop if for any reasons multiple body in the page
                 }
             }
@@ -51,6 +70,7 @@ public class AddStuff extends AbstractFilter implements ApplicationListener<Appl
 
         return out;
     }
+
     public void onApplicationEvent(ApplicationEvent event) {
     }
 }
